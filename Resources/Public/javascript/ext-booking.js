6b4a5a78572646f674d65ca010a057db45889cb6
layen <layenjulian@gmail.com>
2016-04-19 20:19:16 +0200

    #1
    Convert to utf-8 and change date picker to french format

--- Resources/Public/javascript/ext-booking.js
+++ Resources/Public/javascript/ext-booking.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
     $('.datepicker').datepicker({
-        format: 'D M yyyy'
+        days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"],
+        daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam.", "dim."],
+        daysMin: ["d", "l", "ma", "me", "j", "v", "s", "d"],
+        months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+        monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
+        today: "Aujourd'hui",
+        clear: "Effacer",
+        weekStart: 1,
+        format: "dd/mm/yyyy"
     });
 });
